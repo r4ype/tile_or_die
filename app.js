a535d58def8f14aeba834a26350f3bf04b8835cd
r4ype <raypemaster@gmail.com>
2024-12-15 18:46:04 +0330

    basic tile generation and styling

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("javascript is running...");
+let board;
+let score = 0;
+let rows = 4;
+let columns = 4;
+
+window.onload = function () {
+  loadGame();
+};
+
+function loadGame() {
+  board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      let num = board[r][c];
+      updateTile(tile, num);
+      document.getElementById("board").append(tile);
+    }
+  }
+}
+
+function updateTile(tile, num) {
+  tile.innerText = "";
+  tile.classList.value = "";
+  tile.classList.add("tile");
+  if (num > 0) {
+    tile.innerText = num;
+    if (num <= 4096) {
+      tile.classList.add("t" + num.toString());
+    }
+  }
+}
